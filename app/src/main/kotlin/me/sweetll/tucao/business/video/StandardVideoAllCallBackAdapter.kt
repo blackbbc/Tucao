2d6efb9ba760b7e56af716e9ea5d334506af795f
blackbbc <505968815@qq.com>
2017-01-10 00:06:25 +0800

    it worked!

---
+++ app/src/main/kotlin/me/sweetll/tucao/business/video/StandardVideoAllCallBackAdapter.kt
@@ -0,0 +1,89 @@
+package me.sweetll.tucao.business.video
+
+import com.shuyu.gsyvideoplayer.listener.StandardVideoAllCallBack
+
+open class StandardVideoAllCallBackAdapter: StandardVideoAllCallBack {
+    override fun onClickStartThumb(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickBlankFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickBlank(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickResumeFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onEnterFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickResume(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickSeekbarFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickStartIcon(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onTouchScreenSeekLight(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onQuitFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onEnterSmallWidget(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickStartError(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onPrepared(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onAutoComplete(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onQuitSmallWidget(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onTouchScreenSeekVolume(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickStop(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickSeekbar(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onPlayError(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onClickStopFullscreen(p0: String?, vararg p1: Any?) {
+
+    }
+
+    override fun onTouchScreenSeekPosition(p0: String?, vararg p1: Any?) {
+
+    }
+}
--- app/src/main/kotlin/me/sweetll/tucao/business/video/VideoActivity.kt
+++ app/src/main/kotlin/me/sweetll/tucao/business/video/VideoActivity.kt
@@ -2,14 +2,19 @@ package me.sweetll.tucao.business.video
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.view.View
+import com.shuyu.gsyvideoplayer.GSYPreViewManager
+import com.shuyu.gsyvideoplayer.GSYVideoPlayer
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
 import me.sweetll.tucao.R
 import me.sweetll.tucao.base.BaseActivity
 import me.sweetll.tucao.business.video.viewmodel.VideoViewModel
 import me.sweetll.tucao.databinding.ActivityVideoBinding
+import me.sweetll.tucao.extension.toast
 import me.sweetll.tucao.model.json.Result
 import me.sweetll.tucao.model.xml.Durl
 
@@ -20,6 +25,9 @@ class VideoActivity : BaseActivity() {
     lateinit var orientationUtils: OrientationUtils
 
     lateinit var result: Result
+
+    var isPlay = false
+    var isPause = false
 
     companion object {
         private val ARG_RESULT = "result"
@@ -38,6 +46,42 @@ class VideoActivity : BaseActivity() {
         result = intent.getParcelableExtra(ARG_RESULT)
 
         orientationUtils = OrientationUtils(this, binding.player)
+        orientationUtils.isEnable = false
+
+        // 是否可以滑动界面改变进度，声音
+        binding.player.setIsTouchWiget(true)
+        //关闭自动旋转
+        binding.player.isRotateViewAuto = false
+        binding.player.isLockLand = false
+        binding.player.isShowFullAnimation = false
+        binding.player.isNeedLockFull = true
+        binding.player.isOpenPreView = true
+        binding.player.fullscreenButton.setOnClickListener {
+            view ->
+            //直接横屏
+            orientationUtils.resolveByClick()
+
+            //第一个true是否需要隐藏ActionBar，第二个true是否需要隐藏StatusBar
+            binding.player.startWindowFullscreen(this, true, true)
+        }
+
+        binding.player.setStandardVideoAllCallBack(object: StandardVideoAllCallBackAdapter() {
+            override fun onPrepared(p0: String?, vararg p1: Any?) {
+                super.onPrepared(p0, *p1)
+                orientationUtils.isEnable = true
+                isPlay = true
+            }
+
+            override fun onQuitFullscreen(p0: String?, vararg p1: Any?) {
+                super.onQuitFullscreen(p0, *p1)
+                orientationUtils.backToProtVideo()
+            }
+        })
+
+        binding.player.setLockClickListener {
+            view, lock ->
+            orientationUtils.isEnable = !lock
+        }
 
         viewModel.queryPlayUrls(result.video[0].type, result.video[0].vid)
     }
@@ -45,7 +89,40 @@ class VideoActivity : BaseActivity() {
     fun loadDuals(durls: MutableList<Durl>?) {
         durls?.isNotEmpty().let {
             binding.player.setUp(durls!![0].url, true, null)
+            "载入完成".toast()
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        isPause = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isPause = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GSYVideoPlayer.releaseAllVideos()
+        GSYPreViewManager.instance().releaseMediaPlayer()
+        orientationUtils.releaseListener()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (isPlay && !isPause) {
+            if (newConfig.orientation == ActivityInfo.SCREEN_ORIENTATION_USER) {
+                if (!binding.player.isIfCurrentIsFullscreen) {
+                    binding.player.startWindowFullscreen(this, true, true)
+                }
+            } else {
+                if (binding.player.isIfCurrentIsFullscreen) {
+                    StandardGSYVideoPlayer.backFromWindowFull(this);
+                }
+                orientationUtils.isEnable = true
+            }
+        }
+    }
 }
