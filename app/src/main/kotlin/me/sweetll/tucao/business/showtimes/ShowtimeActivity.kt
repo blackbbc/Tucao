2c118580d62210b16ce45e9cd4e2edbc5f1c8227
blackbbc <505968815@qq.com>
2017-01-19 18:49:45 +0800

    some bug

--- app/src/main/kotlin/me/sweetll/tucao/business/showtimes/ShowtimeActivity.kt
+++ app/src/main/kotlin/me/sweetll/tucao/business/showtimes/ShowtimeActivity.kt
@@ -15,7 +15,7 @@ import me.sweetll.tucao.base.BaseActivity
 import me.sweetll.tucao.business.showtimes.adapter.ShowtimeAdapter
 import me.sweetll.tucao.business.showtimes.viewmodel.ShowtimeViewModel
 import me.sweetll.tucao.databinding.ActivityShowtimeBinding
-import me.sweetll.tucao.extension.logD
+import me.sweetll.tucao.extension.dp2px
 import me.sweetll.tucao.model.raw.ShowtimeSection
 import java.util.*
 
@@ -24,7 +24,9 @@ class ShowtimeActivity : BaseActivity() {
     val viewModel: ShowtimeViewModel by lazy { ShowtimeViewModel(this) }
 
     val showtimeAdapter = ShowtimeAdapter(null)
-    val headerPositions: MutableList<Int> = mutableListOf()
+    val headerOffsets: MutableList<Int> = mutableListOf()
+    var totalScroll = 0
+    var currentHeaderView: View? = null
 
     override fun getToolbar(): Toolbar = binding.toolbar
 
@@ -43,7 +45,7 @@ class ShowtimeActivity : BaseActivity() {
         binding.indicatorFrame.viewTreeObserver.addOnGlobalLayoutListener( object: ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 binding.indicatorFrame.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                var dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1
+                var dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2
                 if (dayOfWeek < 0) dayOfWeek = 6
                 val targetView = binding.weekLinear.getChildAt(dayOfWeek)
                 moveIndicatorView(targetView)
@@ -61,8 +63,16 @@ class ShowtimeActivity : BaseActivity() {
         binding.showtimeRecycler.adapter = showtimeAdapter
 
         binding.showtimeRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                dy.toString().logD()
+                totalScroll += dy
+                val currentHeaderIndex = headerOffsets.indexOfLast {
+                    it <= totalScroll
+                }
+                val targetView = binding.weekLinear.getChildAt(currentHeaderIndex)
+                targetView?.let {
+                    moveIndicatorView(it)
+                }
             }
         })
     }
@@ -82,15 +92,26 @@ class ShowtimeActivity : BaseActivity() {
 
     fun loadShowtime(data: MutableList<ShowtimeSection>) {
         showtimeAdapter.setNewData(data)
-        headerPositions.clear()
+        headerOffsets.clear()
+
+        var yOffset = 0f
+        var itemCount = 0
         data.forEachIndexed {
             position, showtimeSection ->
+
             if (showtimeSection.isHeader) {
-                headerPositions.add(position)
+                itemCount = 0
+                headerOffsets.add(yOffset.toInt())
+                yOffset += 60f.dp2px()
+            } else {
+                itemCount = (itemCount % 3) + 1
+                if (itemCount == 1) {
+                    yOffset += 150f.dp2px()
+                }
             }
         }
 
-        var dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1
+        var dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2
         if (dayOfWeek < 0) dayOfWeek = 6
         showWeek(dayOfWeek)
 
@@ -98,19 +119,22 @@ class ShowtimeActivity : BaseActivity() {
     }
 
     fun moveIndicatorView(targetView: View) {
-        binding.indicatorView.animate()
-                .x(targetView.x + targetView.width / 2f - binding.indicatorView.width / 2f)
-                .setInterpolator(AccelerateDecelerateInterpolator())
-                .setDuration(200)
-                .start()
+        if (targetView != currentHeaderView) {
+            currentHeaderView = targetView
+            binding.indicatorView.animate()
+                    .x(targetView.x + targetView.width / 2f - binding.indicatorView.width / 2f)
+                    .setInterpolator(AccelerateDecelerateInterpolator())
+                    .setDuration(200)
+                    .start()
+        }
     }
 
     fun showWeek(week: Int, view: View? = null) {
         view?.let {
             moveIndicatorView(it)
         }
-        if (headerPositions.isNotEmpty()) {
-            binding.showtimeRecycler.smoothScrollToPosition(headerPositions[week])
+        if (headerOffsets.isNotEmpty()) {
+            binding.showtimeRecycler.smoothScrollBy(0, headerOffsets[week] - totalScroll)
         }
     }
 
--- app/src/main/kotlin/me/sweetll/tucao/extension/FloatExtensions.kt
+++ app/src/main/kotlin/me/sweetll/tucao/extension/FloatExtensions.kt
@@ -1,3 +1,11 @@
 package me.sweetll.tucao.extension
 
+import android.util.TypedValue
+import me.sweetll.tucao.AppApplication
+
 fun Float.isPercentageNumber() = this >= 0f && this <= 1f
+
+fun Float.dp2px(): Float {
+    val r = AppApplication.get().resources
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, r.displayMetrics)
+}
