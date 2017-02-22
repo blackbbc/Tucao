e157d2dbe23728b9ebc665957fbc18797f933946
blackbbc <505968815@qq.com>
2017-02-22 20:32:34 +0800

    Fix parse fail bug

--- app/src/main/kotlin/me/sweetll/tucao/di/service/RawApiService.kt
+++ app/src/main/kotlin/me/sweetll/tucao/di/service/RawApiService.kt
@@ -2,9 +2,7 @@ package me.sweetll.tucao.di.service
 
 import io.reactivex.Observable
 import okhttp3.ResponseBody
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface RawApiService {
     @GET(ApiConfig.DANMU_API_URL)
@@ -12,12 +10,15 @@ interface RawApiService {
               @Query("r") r: Long) : Observable<ResponseBody>
 
     @GET(ApiConfig.INDEX_URL)
+    @Headers("Cookie: tucao_verify=ok")
     fun index() : Observable<ResponseBody>
 
     @GET(ApiConfig.LIST_URL)
+    @Headers("Cookie: tucao_verify=ok")
     fun list(@Path("tid") tid: Int): Observable<ResponseBody>
 
     @GET(ApiConfig.BGM_URL)
+    @Headers("Cookie: tucao_verify=ok")
     fun bgm(@Path("year") year: Int,
             @Path("month") month: Int): Observable<ResponseBody>
 }
