18e336a6f1dcfd32c482cf28ec6a1f7b10600995
blackbbc <505968815@qq.com>
2017-07-08 17:15:55 +0800

    WIP

--- app/src/main/kotlin/me/sweetll/tucao/business/login/LoginActivity.kt
+++ app/src/main/kotlin/me/sweetll/tucao/business/login/LoginActivity.kt
@@ -2,20 +2,38 @@ package me.sweetll.tucao.business.login
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
 
 import me.sweetll.tucao.R
 import me.sweetll.tucao.base.BaseActivity
+import me.sweetll.tucao.databinding.ActivityLoginBinding
 
 class LoginActivity : BaseActivity() {
-    override fun initView(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_login)
-    }
+    lateinit var binding: ActivityLoginBinding
+
+    override fun getStatusBar(): View = binding.statusBar
+
+    override fun getToolbar(): Toolbar = binding.toolbar
 
     companion object {
         fun intentTo(context: Context) {
             val intent = Intent(context, LoginActivity::class.java)
             context.startActivity(intent)
+        }
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+
+    override fun initToolbar() {
+        super.initToolbar()
+        supportActionBar?.let {
+            it.title = "登陆"
+            it.setDisplayHomeAsUpEnabled(true)
         }
     }
 
