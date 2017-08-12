07b9a68445627434ff2f36266b50ebb54867da3f
Luo Yi <ext.yi.luo@uaes.com>
2017-08-12 21:25:18 +0800

    WIP

--- app/src/main/kotlin/me/sweetll/tucao/business/personal/PersonalActivity.kt
+++ app/src/main/kotlin/me/sweetll/tucao/business/personal/PersonalActivity.kt
@@ -1,13 +1,40 @@
 package me.sweetll.tucao.business.personal
 
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
 import me.sweetll.tucao.R
+import me.sweetll.tucao.base.BaseActivity
+import me.sweetll.tucao.databinding.ActivityPersonalBinding
 
-class PersonalActivity : AppCompatActivity() {
+class PersonalActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personal)
+    private lateinit var binding: ActivityPersonalBinding
+
+    override fun getStatusBar(): View = binding.statusBar
+
+    override fun getToolbar(): Toolbar = binding.toolbar
+
+    companion object {
+        fun intentTo(context: Context) {
+            val intent = Intent(context, PersonalActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_personal)
+    }
+
+    override fun initToolbar() {
+        super.initToolbar()
+        supportActionBar?.let {
+            it.title = "修改资料"
+            it.setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
---
+++ app/src/main/kotlin/me/sweetll/tucao/business/personal/fragment/ChangeInfomationFragment.kt
@@ -0,0 +1,17 @@
+package me.sweetll.tucao.business.personal.fragment
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChangeInfomationFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
